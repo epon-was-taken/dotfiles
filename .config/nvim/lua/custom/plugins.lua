af4f6bb54b9d2bcf8a6b09b22c2a9f997c5e15fb
Patrick Reinboth <preinboth@mtg.de>
2024-01-19 09:48:32 +0100

    added a bunch of plugins

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,6 +1,31 @@
 local plugins = {
-  {
 
-  }
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    opts = {}
+  },
+
+  {
+    'pearofducks/ansible-vim',
+    lazy = false
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    config = function ()
+      require 'plugins.configs.lspconfig'
+      require 'custom.configs.lspconfig'
+    end,
+  },
+
+  {
+    'ErichDonGubler/lsp_lines.nvim',
+    config = function ()
+      require('ls_lines').setup()
+    end,
+  },
+
 }
+
 return plugins
