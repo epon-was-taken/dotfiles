3801bd208fd43d202aa43d68f46621ad8485dfa7
Patrick Reinboth <50664702+PatrickReinboth@users.noreply.github.com>
2024-06-12 18:22:50 +0200

    changed setup function

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -44,16 +44,17 @@ return {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dapui.listeners.after.event_initialized["dapui_config"] = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
         dapui.open()
       end
-      dapui.listeners.after.event_terminated["dapui_config"] = function()
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
       end
-      dapui.listeners.after.event_exited["dapui_config"] = function()
+      dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
     end
