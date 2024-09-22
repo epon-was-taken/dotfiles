739ffeb9f38cf692a3bc7032ecee0866ade95a9f
Patrick Reinboth <50664702+PatrickReinboth@users.noreply.github.com>
2024-09-22 21:58:56 +0200

    added better mappings to select stuff

--- .config/nvim/lua/plugins/cmp_config.lua.lua
+++ .config/nvim/lua/plugins/cmp_config.lua.lua
@@ -13,9 +13,11 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
+          ["<C-l>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },   -- LSP source for autocompletion
