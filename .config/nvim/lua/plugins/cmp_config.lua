9a9d7d81e0aac0a024b747980854d9bfa3ee92d7
Patrick Reinboth <50664702+PatrickReinboth@users.noreply.github.com>
2024-10-29 11:10:21 +0100

    nvim changes

--- .config/nvim/lua/plugins/cmp_config.lua
+++ .config/nvim/lua/plugins/cmp_config.lua
@@ -11,13 +11,11 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-l>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm completion only when tabbed in.
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },   -- LSP source for autocompletion
@@ -29,6 +27,7 @@ return {
       })
     end,
   },
+
 
   -- LSP source for nvim-cmp
   {
