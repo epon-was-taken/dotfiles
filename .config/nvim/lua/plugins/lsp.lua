cdeda89b70bd1ecfbee2e50d23ec991bf4ba2342
Patrick Reinboth <patrick.reinboth@mtg.de>
2025-10-29 12:04:45 +0100

    added neovim conf to dotfiles

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,48 @@
+return {
+  -- Install Mason Packages
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "ansible-language-server",
+        "ansible-lint",
+        "bash-language-server",
+        "shfmt",
+        "gopls",
+        "golangci-lint",
+        "gofumpt",
+      },
+    },
+  },
+
+  -- Set formatters by filetype
+  {
+    "stevearc/conform.nvim",
+    opts = function()
+      local opts = {
+        formatters_by_ft = {
+          lua = { "stylua" },
+          sh = { "shfmt" },
+          go = { "gofumpt" },
+        },
+      }
+      return opts
+    end,
+  },
+
+  -- Ansible filetype detection and useful functions
+  {
+    "mfussenegger/nvim-ansible",
+    event = { "VeryLazy" },
+    keys = {
+      {
+        "<leader>te",
+        function()
+          require("ansible").run()
+        end,
+        desc = "Run Ansible Playbook",
+        { buffer = true, silent = true },
+      },
+    },
+  },
+}
