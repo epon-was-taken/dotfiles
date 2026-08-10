a3fbfeceb406e2d95cedc7ca72dc2883d27f1fbb
Patrick Reinboth <preinboth@wotan.asgaard.dev>
2026-08-10 19:55:44 +0200

    changed kitty conf and tmux conf

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -1,4 +1,7 @@
 export EDITOR=nvim
+export YOUTUBE_API_KEY=AIzaSyDm7u4LPXpRZktBZU3sLWH4V_sUjPutyqM
+export TELEGRAM_BOT_TOKEN="8856849351:AAGKJQscinWp6fb1mA6w64iJtmm3Qc6qhiE"
+export TELEGRAM_CHAT_ID="776632009"
 
 # User specific environment
 for dir in "$HOME/.local/bin" "$HOME/bin" "$HOME/go/bin" "$HOME/.cargo/bin"; do
